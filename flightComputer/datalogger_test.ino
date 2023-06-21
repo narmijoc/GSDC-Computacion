804638d9fd7c92562a2642ceb5286e06033019a3
uwo-o <hugo.campos@usm.cl>
2023-06-20 23:30:10 -0400

    Files ordened and conditions re structured

--- flightComputer/datalogger_test.ino
+++ flightComputer/datalogger_test.ino
@@ -1,47 +1,41 @@
-/*
- *  Arduino SD Card Tutorial Example
- *  
- *  by Dejan Nedelkovski, www.HowToMechatronics.com
- */
-
 #include <SD.h>
 #include <SPI.h>
 
 File myFile;
 
+const int SERIAL_PORT_SPEED = 9600;
+
 int pinCS = 10; // Pin 10 on Arduino Uno
 
 void setup() {
-    
-  Serial.begin(9600);
+
+  Serial.begin(SERIAL_PORT_SPEED);
   pinMode(pinCS, OUTPUT);
-  
+
   // SD Card Initialization
-  if (SD.begin())
-  {
-    Serial.println("SD card is ready to use.");
-  } else
-  {
-    Serial.println("SD card initialization failed");
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
     return;
   }
-  
-  // Create/Open file 
+
+  // Create/Open file
   myFile = SD.open("test.txt", FILE_WRITE);
-  
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.println("Writing to file...");
-    // Write to file
-    myFile.println("Testing text 1, 2 ,3...");
-    myFile.close(); // close the file
-    Serial.println("Done.");
-  }
+
   // if the file didn't open, print an error:
-  else {
-    Serial.println("error opening test.txt");
+  if (!myFile){
+    Serial.println("File open failed!");
+    return;
   }
-/*
+
+  // If the file opened okay, write to it:
+  Serial.println("Writing to file...");
+
+  // Write to file
+  myFile.println("Testing text 1, 2 ,3...");
+  myFile.close(); // close the file
+  Serial.println("Done.");
+
+  /*
   // Reading the file
   myFile = SD.open("test.txt");
   if (myFile) {
@@ -56,7 +50,6 @@ void setup() {
     Serial.println("error opening test.txt");
   }
   */
-  
 }
 void loop() {
   // empty
